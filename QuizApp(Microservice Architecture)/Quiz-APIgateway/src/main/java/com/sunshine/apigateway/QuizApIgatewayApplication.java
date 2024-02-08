989747ba193d72181a2273154386bbe2f405b84d
aadiisawant <adisawant0178@gmail.com>
2024-02-08 17:17:52 +0530

    API gateway service added

---
+++ QuizApp(Microservice Architecture)/Quiz-APIgateway/src/main/java/com/sunshine/apigateway/QuizApIgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sunshine.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApIgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApIgatewayApplication.class, args);
+	}
+
+}
